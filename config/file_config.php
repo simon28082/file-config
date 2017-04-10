683878b0d130f1c798139cd9d2fcd82650a8ad20
simon <crcms@crcms.cn>
2017-04-10 10:02:03 +0800

    config

---
+++ config/file_config.php
@@ -0,0 +1,28 @@
+<?php
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | config files
+    |--------------------------------------------------------------------------
+    | The file name (not including the extension) must be unique
+    | Example
+    | files => [
+    |    \CrCms\FileConfig\Drives\JsonConfig::class=>'json.conf',
+    |    \CrCms\FileConfig\Drives\SerializeConfig::class=>'serialize.txt',
+    |
+    | ]
+    */
+    'files'=> [
+
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | default drive
+    |--------------------------------------------------------------------------
+    | The default driver, when the index is a natural index, uses this driver
+    |
+    */
+    'default_drive'=>\CrCms\FileConfig\Drives\DefaultConfig::class,
+];

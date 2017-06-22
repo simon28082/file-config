0d2647f774dc3d4ef1f691a502f4222a2ad60702
simon <crcms@crcms.cn>
2017-06-23 07:52:54 +0800

    增加文件名获取整个数组

--- src/helper.php
+++ src/helper.php
@@ -2,16 +2,16 @@
 use CrCms\FileConfig\Factory;
 
 /**
- * @param null $key
+ * @param array|string|null $key
  * @param string $default
  * @param array $config
- * @return bool|\CrCms\FileConfig\FileConfig|string
+ * @return array|\CrCms\FileConfig\FileConfig|string
  */
 function file_config($key = null, string $default = '', array $config = [])
 {
 
     if (empty($config)) {
-        $config = function_exists('app') && is_object(app('config')) ? app('config')->get('file_config') : require realpath(__DIR__.DIRECTORY_SEPARATOR.'../config/file_config.php');
+        $config = function_exists('app') && is_object(app('config')) ? app('config')->get('file_config') : require realpath(__DIR__ . DIRECTORY_SEPARATOR . '../config/file_config.php');
     }
 
     if (is_null($key)) {
@@ -19,10 +19,12 @@ function file_config($key = null, string $default = '', array $config = [])
     }
 
     if (is_array($key)) {
-        return array_walk($key,function($item,$k) use ($config){
-            Factory::fileConfig($config)->put($k,$item);
+        return array_walk($key, function ($item, $k) use ($config) {
+            Factory::fileConfig($config)->put($k, $item);
         });
     }
 
-    return Factory::fileConfig($config)->get($key, $default);
+    return strpos($key, '.') === false ?
+        Factory::fileConfig($config)->all($key) :
+        Factory::fileConfig($config)->get($key, $default);
 }

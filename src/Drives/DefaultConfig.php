d8fb5bd40e51ef0228f5df27392e7aca52116587
simon <crcms@crcms.cn>
2017-06-23 07:53:42 +0800

    格式化&合并容错方式

--- src/Drives/DefaultConfig.php
+++ src/Drives/DefaultConfig.php
@@ -9,43 +9,33 @@ use CrCms\FileConfig\Contracts\FormatConfig;
  */
 class DefaultConfig implements FormatConfig
 {
-
     /**
      * @var string
      */
     protected $delimiter = "\n";
 
-
     /**
      * @param string $content
      * @return array
      */
-    public function read(string $content) : array
+    public function read(string $content): array
     {
-        $content = explode($this->delimiter,$content);
+        $content = explode($this->delimiter, $content);
 
         $array = [];
-
         foreach ($content as $value) {
-
             $value = trim($value);
 
-            if (empty($value) || strpos($value,'#') === 0) {
+            if (empty($value) || strpos($value, '#') === 0 || strpos($value, '=') === false) {
                 continue;
             }
 
-            $value = explode('=',$value);
-
-            if (count($value) !== 2) {
-                continue;
-            }
-
+            $value = explode('=', $value);
             $array[trim($value[0])] = trim($value[1]);
         }
 
         return $array;
     }
-
 
     /**
      * @param array $content
@@ -54,10 +44,10 @@ class DefaultConfig implements FormatConfig
     public function write(array $content): string
     {
         $string = '';
-        foreach ($content as $key=>$value) {
+        foreach ($content as $key => $value) {
             $string .= "{$key}={$value}\n";
         }
-        return rtrim($string,"\n");
-    }
 
+        return rtrim($string, "\n");
+    }
 }
